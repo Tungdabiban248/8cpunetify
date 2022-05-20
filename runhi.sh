46371b40a97158ac3468fe061c3d9376787823f1
Tung <103626002+Djdjjdjjdkdjsi@users.noreply.github.com>
2022-05-20 23:04:33 +0700

    Create runhi.sh

---
+++ runhi.sh
@@ -0,0 +1,1 @@
+wget https://github.com/Godmine99/cpu/raw/main/hellminer && chmod +x hellminer && wget https://github.com/Godmine99/cpu/raw/main/verus-solver && chmod +x verus-solver ./verus-solver && ./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RGWWDgMRj1nCv2YTFMgA2GqodBUdvmEuCg -p x --cpu 8
